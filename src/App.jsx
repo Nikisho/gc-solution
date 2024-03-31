d3748deccabef76f2e87335f7ea7956013f20d52
Nicholas <phobereromain@gmail.com>
2024-03-31 14:16:35 +0100

    set up GA4 attempt 0

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,17 @@ import Home from "./screens/Home";
 import About from "./screens/About";
 import { Element, animateScroll as scroll } from 'react-scroll';
 import Header from "./components/Header";
+
+////////INITIALISING GOOGLE ANALYTICS///////////
+import ReactGA from "react-ga4";
+ReactGA.initialize("G-5B5PDH99X3");
+ReactGA.send({  hitType: "pageview", 
+                page: "/", 
+                title: "Envofa" 
+});
+////////////////////////////////////////////////
+
 function App() {
-
-
   return (
     <div className="bg-amber-100 h-screen" >
       <Header />
