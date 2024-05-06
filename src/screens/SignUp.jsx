6c63843b587a0ea363ca7074b916ec47ae152fb1
Nicholas <phobereromain@gmail.com>
2024-05-06 13:25:45 +0100

    changed size for mobile

--- src/screens/SignUp.jsx
+++ src/screens/SignUp.jsx
@@ -4,7 +4,7 @@ import SignUpComponent from '../components/SignUpComponent'
 function SignUp() {
     return (
         <div className=' h-screen flex flex-col items-center justify-center space-y-2 px-5 w-full  '>
-            <div className='w-4/5 flex flex-col items-center justify-center space-y-5'>
+            <div className='xl:w-4/5 flex flex-col items-center justify-center space-y-5'>
 
                 <div className='p-2 xl:p-0 xl:px-2 xl:w-1/2 font-sans text-xl 2xl:w-1/3 '>
                     We haven’t launched yet, but we are admitting people into the beta program.
